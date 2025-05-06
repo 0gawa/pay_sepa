d9ec05c4964b83f81350b9f186deb4005432cf92
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2025-05-06 20:35:03 +0900

    groupコントローラーの作成

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    resources :users, only: [:create, :destroy]
+    resources :users,        only: [:create, :destroy]
+    resources :groups,       only: [:create, :destroy]
     resources :transactions, only: [:index, :create, :destroy]
-    resources :settlements, only: [:index, :create, :destroy]
-    resources :balances, only: [:index] 
+    resources :settlements,  only: [:index, :create, :destroy]
+    resources :balances,     only: [:index] 
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
