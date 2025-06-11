369a451d39a83ef081cc0ff63d8b5596defd38bd
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2025-06-11 20:25:00 +0900

    グループメンバーコンポーネントの表示を作成

--- front/app/(app)/group/[id]/page.tsx
+++ front/app/(app)/group/[id]/page.tsx
@@ -1,17 +1,36 @@
+'use client';
+
+import { useState } from 'react';
 import Transactions from '@/app/ui/group/transactions';
 import Users from '@/app/ui/group/users';
 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
-  const params = await props.params;
-  const group_id = params.id
+interface Member {
+  id: number;
+  name: string;
+}
+
+export default function Page(props: { params: Promise<{ id: string }> }) {
+  const [members, setMembers] = useState<Member[]>([]);
+
+  const handleAddMember = (name: string) => {
+    const newMember = { id: 1, name };
+    setMembers((prev) => [...prev, newMember]);
+  };
+  const handleDeleteMember = (id: number) => {
+    setMembers((prev) => prev.filter((member) => member.id !== id));
+  };
 
   return (
     <div className="min-h-screen justify-center p-4 sm:p-6 lg:p-8">
       <div>
-        <Transactions groupId={group_id} />
+        {/* <Transactions groupId={group_id} /> */}
       </div>
       <div>
-        <Users groupId={group_id} />
+        <Users
+          members={members}
+          onAddMember={handleAddMember}
+          onDeleteMember={handleDeleteMember}
+        />
       </div>
       <div>
 
