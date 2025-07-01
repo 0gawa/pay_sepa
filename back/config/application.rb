95b431f9429e1e332bf4edefc65bbe98236fe48b
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2025-07-02 00:59:41 +0900

    ホストの許可

--- back/config/application.rb
+++ back/config/application.rb
@@ -28,5 +28,7 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts << "just-pay-9bzu.onrender.com"
   end
 end
